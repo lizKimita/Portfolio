7fcd24bed01655c52224d56eb3e34f66adb5880c
lizKimita <kimita.wanjiku@gmail.com>
2019-05-11 21:35:06 +0300

    adding initial file structure

---
+++ kimita/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KimitaConfig(AppConfig):
+    name = 'kimita'
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/elizabeth/Documents/Moringa-projects/core-projects/Django/Portfolio/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
